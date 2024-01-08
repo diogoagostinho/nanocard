9fe64725c9e28dd98e3648fcbb02686a3a1cafe7
Diogo Agostinho <laagostim.oficial@gmail.com>
2024-01-08 22:26:38 +0000

    r0.1.1
    Added a few details

---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <div className="header__hero"></div>
+    </>
+  );
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Home";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div className="content">
+      <div className="navbar">
+        <div className="navbar__left">
+          <h3 className="navbar-title">
+            <span className="bold-text">Nano</span>Card
+          </h3>
+        </div>
+        <div className="navbar__right"></div>
+      </div>
+      <Home />
+    </div>
+  </React.StrictMode>
+);
